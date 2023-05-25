99f4b7f14417bcd6dd9fccbceeac85e72d055882
KristupasNo <76537530+KristupasNo@users.noreply.github.com>
2023-05-25 18:33:53 +0300

    Update CustomerController.java

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/V1/customer")
+@CrossOrigin("*")
 public class CustomerController {
     private final CustomerService customerService;
     @Autowired
