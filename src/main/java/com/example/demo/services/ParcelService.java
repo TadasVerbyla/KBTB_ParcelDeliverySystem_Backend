cd9e9912a919d6efd5bde58e41f21277e4b51bc1
Gedas1357 <gedasvilnietis@gmail.com>
2023-05-27 10:18:02 +0300

    Delete App.js

--- src/main/java/com/example/demo/services/ParcelService.java
+++ src/main/java/com/example/demo/services/ParcelService.java
@@ -36,7 +36,7 @@ public class ParcelService {
     public Parcel getParcel(Long parcelId){
         return parcelRepository.findById(parcelId).orElseThrow(() -> new IllegalStateException("Specified entry does not exist. "));
     };
-    
+
     @Transactional
     public void addNewParcel(Parcel parcel) {
         parcelRepository.save(parcel);
